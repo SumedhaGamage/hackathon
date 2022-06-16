4cbd123d5ae51145bf187f61bcbe80f4f4833655
Sumedha Gamage <sumedhagamage@gmail.com>
2022-06-15 22:11:26 -0500

    add boto3

--- code/pii_checker/pii_checker.py
+++ code/pii_checker/pii_checker.py
@@ -4,10 +4,12 @@ import boto3
 
 def handler(event, context):
     request = json.loads(event['body'])
+    text = request['text']
 
     # add comprehend code here
+    response = {'result': text}
 
     return {
         'statusCode': 200,
-        'body': json.dumps(request)
+        'body': json.dumps(response)
     }
