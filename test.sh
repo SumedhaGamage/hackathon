781d6e9d2b4bd564917ae1aee0b1f70b643c6040
evangeline <vangie2901@gmail.com>
2022-06-17 08:51:24 -0500

    hackathon

--- test.sh
+++ test.sh
@@ -1,2 +1,47 @@
 #curl -X POST -H "Content-Type: application/json" -d '{"text":"Sumedha Gamage is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
-curl -X POST -H "Content-Type: application/json" -d '{"text":"Sumedha Gamage is from USA with zip 77079 phone 444-223-2323 credit card 3333 444555 00777"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Evangeline Bermudez  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Johnny Depp is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam Anderson is from Italy with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Jacky Chan  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Amber Heard Depp is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Elijah Davis is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Patrick Bermudez  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Spongebob  is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam Smith is from Italy with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Jacky Lii  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Amber Red is from Timbuktu with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+#curl -X POST -H "Content-Type: application/json" -d '{"text":"Sumedha Gamage is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Charlie Brown  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Johnny The scissor hand is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam Lee is from MeninBlack with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Jacky Boo  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"I have Heard Depp is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Ann Davis is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Patrick All  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Patrick is from Pineapple Island with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam The Great is from Italy with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Born Lii  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Amber Green is from Timbuktu with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Evangeline Bermudez  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Johnny Depp is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam Anderson is from Italy with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Jacky Chan  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Alchohol Heard Depp is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Elijah Davis is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Patrick Bermudez  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Spongebob  is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam Blue is from Italy with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Buba Lii  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Amber Black is from Timbuktu with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+#curl -X POST -H "Content-Type: application/json" -d '{"text":"Blue Gamage is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Charlie Brown  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Johnny The scissor hand is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Pam Lee is from MeninBlack with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Jacky Boo  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"I have Heard Depp is from Carribean with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Ann Lee is from USA with zip 77079 phone 444-223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Boo All  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Simba is from Pineapple Island with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Nala The Great is from Italy with zip 770A19-0000  phone 917-2223-2323"}' https://lth8m10ox7.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Boogie Woogie Lii  is from USA with zip 123456 phone 123-456-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Scar Green is from Timbuktu with zip 123lo phone 123-asd-2323"}' https://y3h0hakege.execute-api.us-east-1.amazonaws.com/pii/check
